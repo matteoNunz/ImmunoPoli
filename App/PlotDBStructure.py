c7bd17314488e810546cab877b6d6171345cce12
margheritamusumeci <marghe.musumeci@gmail.com>
2021-11-11 18:38:11 +0100

    Added method for loading graphs on mac os

--- App/PlotDBStructure.py
+++ App/PlotDBStructure.py
@@ -1,4 +1,7 @@
 from pyvis.network import Network
+import webbrowser
+import platform
+import os
 
 
 class PlotDBStructure:
@@ -8,12 +11,13 @@ class PlotDBStructure:
     Call the addStructure method to add nodes/relationships
     Call the showGraph method to show the html file
     """
+
     @staticmethod
     def __init__():
         """
         Method that creates and initialize the network attribute
         """
-        PlotDBStructure.network = Network('500px' , '500px' , directed = True)
+        PlotDBStructure.network = Network('500px', '500px', directed=True)
         PlotDBStructure.personColor = 'orange'
         PlotDBStructure.locationColor = 'red'
         PlotDBStructure.houseColor = 'blue'
@@ -29,7 +33,7 @@ class PlotDBStructure:
         PlotDBStructure.infectedLocationColor = 'green'
 
     @staticmethod
-    def addStructure(listOfNodesAndArcs = None):
+    def addStructure(listOfNodesAndArcs=None):
         """
         Method that given a list of nodes/relationships adds the corresponding representation in the network
         :param listOfNodesAndArcs: list of nodes or arcs or both
@@ -39,62 +43,62 @@ class PlotDBStructure:
 
         # Create nodes and arcs
         for node in listOfNodesAndArcs:
-            print("Node received is: " , node)
+
             # If Person node
             if 'p' in node.keys():
-                # print("Person node")
+
                 # Add Person nodes
                 label = node["ID(p)"]
                 title = node['p']['name'] + " " + node['p']['surname'] + "," \
                         + node['p']['age'] + "," + node['p']['mail'] + "," \
                         + node['p']['number'] + ",app:" + node['p']['app']
                 color = PlotDBStructure.personColor
-                PlotDBStructure.network.add_node(node["ID(p)"] , label = label , title = title , color = color)
+                PlotDBStructure.network.add_node(node["ID(p)"], label=label, title=title, color=color)
 
             # If Location node
             elif 'l' in node.keys():
-                # print("Location node")
+
                 # Add Location node
                 if 'rate' in node.keys():
-                    label = node['ID(l)'] + str(node['rate'])
+                    label = str(node['ID(l)']) + str(node['rate'])
                 else:
                     label = node['ID(l)']
                 title = node['l']['name'] + "," + node['l']['address'] + "," + node['l']['civic_number'] + "," \
                         + node['l']['CAP'] + "," + node['l']['city'] + "," + node['l']['province'] + "," \
                         + node['l']['type']
                 color = PlotDBStructure.locationColor
-                PlotDBStructure.network.add_node(node['ID(l)'] , label = label , title = title , color = color)
+                PlotDBStructure.network.add_node(node['ID(l)'], label=label, title=title, color=color)
 
             # If House node
             elif 'h' in node.keys():
-                # print("House node")
+
                 # Add House nodes
                 label = node['ID(h)']
                 title = node['h']['name']
                 color = PlotDBStructure.houseColor
-                PlotDBStructure.network.add_node(node['ID(h)'] , label= label , title = title , color = color)
+                PlotDBStructure.network.add_node(node['ID(h)'], label=label, title=title, color=color)
 
             # If Vaccine node
             elif 'v' in node.keys():
-                # print("Vaccine node")
+
                 # Add Vaccine nodes
                 label = node['ID(v)']
                 title = node['v']['name'] + "," + node['v']['producer']
                 color = PlotDBStructure.vaccineColor
-                PlotDBStructure.network.add_node(node['ID(v)'] , label = label , title = title , color = color)
+                PlotDBStructure.network.add_node(node['ID(v)'], label=label, title=title, color=color)
 
             # If Test node
             elif 't' in node.keys():
-                # print("Test node")
+
                 # Add Test nodes
                 label = node['ID(t)']
                 title = node['t']['name']
                 color = PlotDBStructure.testColor
-                PlotDBStructure.network.add_node(node['ID(t)'] , label = label , title = title , color = color)
+                PlotDBStructure.network.add_node(node['ID(t)'], label=label, title=title, color=color)
 
             # If it's a relationship
             elif 'r' in node.keys():
-                # print("A relationship")
+
                 relationship = node  # Just conceptual
                 # Take the ids of the nodes
                 id1 = relationship['ID(n1)']
@@ -104,7 +108,7 @@ class PlotDBStructure:
 
                 if rType == 'LIVE':
                     color = PlotDBStructure.liveColor
-                    PlotDBStructure.network.add_edge(id1 , id2 , title = rType , color = color)
+                    PlotDBStructure.network.add_edge(id1, id2, title=rType, color=color)
                 elif rType == 'APP_CONTACT':
                     if 'r.hour' in relationship.keys():
                         hour = str(relationship['r.hour']).split('.')[0]
@@ -112,124 +116,124 @@ class PlotDBStructure:
                         hour = None
                     title = rType + ",date: " + str(relationship['r.date']) + ",hour: " + hour
                     color = PlotDBStructure.appContactColor
-                    PlotDBStructure.network.add_edge(id1 , id2 , title = title , color = color)
+                    PlotDBStructure.network.add_edge(id1, id2, title=title, color=color)
                 elif rType == 'VISIT':
                     start_hour = str(relationship['r.start_hour']).split('.')[0]
                     end_hour = str(relationship['r.end_hour']).split('.')[0]
                     title = rType + ",date: " + str(relationship['r.date']) + ",start_hour: " \
                             + start_hour + ",end_hour: " + end_hour
                     color = PlotDBStructure.visitColor
-                    PlotDBStructure.network.add_edge(id1 , id2 , title = title , color = color)
+                    PlotDBStructure.network.add_edge(id1, id2, title=title, color=color)
                 elif rType == 'GET':
                     title = rType + ",date: " + str(relationship['r.date']) + ",expiration_date: " \
                             + str(relationship['r.expirationDate']) + ",country: " + relationship['r.country']
                     color = PlotDBStructure.getVaccineColor
-                    PlotDBStructure.network.add_edge(id1 , id2 , title = title , color = color)
+                    PlotDBStructure.network.add_edge(id1, id2, title=title, color=color)
                 elif rType == 'MAKE_TEST':
                     hour = str(relationship['r.hour']).split('.')[0]
                     title = rType + ",date: " + str(relationship['r.date']) + ",hour: " + hour \
                             + ",result: " + relationship['r.result']
                     color = PlotDBStructure.makeTestColor
-                    PlotDBStructure.network.add_edge(id1 , id2 , title = title , color = color)
+                    PlotDBStructure.network.add_edge(id1, id2, title=title, color=color)
                 elif rType == 'COVID_EXPOSURE':
                     title = rType + ",date: " + str(relationship['r.date']) + ",place: " + str(relationship['r.name'])
                     if relationship['r.name'] is None:
                         color = PlotDBStructure.infectedFamilyAndAppColor
                     else:
                         color = PlotDBStructure.infectedLocationColor
-                    PlotDBStructure.network.add_edge(id1 , id2 , title = title , color = color)
+                    PlotDBStructure.network.add_edge(id1, id2, title=title, color=color)
                 else:
                     pass
 
     @staticmethod
-    def addLiveRelationships(pId , hId):
+    def addLiveRelationships(pId, hId):
         """
         Method that adds the relationships LIVE
         :param pId: is the id of the Person
         :param hId: is the id of the House
         """
         color = PlotDBStructure.liveColor
-        PlotDBStructure.network.add_edge(pId, hId, title = 'LIVE' , color = color)
+        PlotDBStructure.network.add_edge(pId, hId, title='LIVE', color=color)
 
     @staticmethod
-    def setPersonColor(color = 'orange'):
+    def setPersonColor(color='orange'):
         """
         Method to set the color of the nodes in the graph
         """
         PlotDBStructure.personColor = color
 
     @staticmethod
-    def setHouseColor(color = 'blue'):
+    def setHouseColor(color='blue'):
         """
         Method to set the color of the nodes in the graph
         """
         PlotDBStructure.houseColor = color
 
     @staticmethod
-    def setLocationColor(color = 'red'):
+    def setLocationColor(color='red'):
         """
         Method to set the color of the nodes in the graph
         """
         PlotDBStructure.locationColor = color
 
     @staticmethod
-    def setVaccineColor(color = 'gray'):
+    def setVaccineColor(color='gray'):
         """
         Method to set the color of the nodes in the graph
         """
         PlotDBStructure.vaccineColor = color
 
     @staticmethod
-    def setTestColor(color = 'green'):
+    def setTestColor(color='green'):
         """
         Method to set the color of the nodes in the graph
         """
         PlotDBStructure.testColor = color
 
     @staticmethod
-    def setLiveColor(color = 'black'):
+    def setLiveColor(color='black'):
         """
         Method to set the color of the relationships in the graph
         """
         PlotDBStructure.liveColor = color
 
     @staticmethod
-    def setVisitColor(color = 'black'):
+    def setVisitColor(color='black'):
         """
         Method to set the color of the relationships in the graph
         """
         PlotDBStructure.visitColor = color
 
     @staticmethod
-    def setAppContactColor(color = 'orange'):
+    def setAppContactColor(color='orange'):
         """
         Method to set the color of the relationships in the graph
         """
         PlotDBStructure.appContactColor = color
 
     @staticmethod
-    def setGetVaccineColor(color = 'black'):
+    def setGetVaccineColor(color='black'):
         """
         Method to set the color of the relationships in the graph
         """
         PlotDBStructure.getVaccineColor = color
 
     @staticmethod
-    def setMakeTestColor(color = 'black'):
+    def setMakeTestColor(color='black'):
         """
         Method to set the color of the relationships in the graph
         """
         PlotDBStructure.makeTestColor = color
 
     @staticmethod
-    def setInfectedFamilyAndAppColor(color = 'red'):
+    def setInfectedFamilyAndAppColor(color='red'):
         """
         Method to set the color of the relationships in the graph
         """
         PlotDBStructure.infectedFamilyAndAppColor = color
 
     @staticmethod
-    def setInfectedLocationColor(color = 'blue'):
+    def setInfectedLocationColor(color='blue'):
         """
         Method to set the color of the relationships in the graph
         """
@@ -246,4 +250,10 @@ class PlotDBStructure:
             print("Empty graph: nothing to show!")
             return
         PlotDBStructure.network.set_edge_smooth('dynamic')
-        PlotDBStructure.network.show('graph.html')
+
+        if platform.system() == "Darwin":  # check if on OSX
+            file_location = "file://" + os.path.abspath('graph.html')
+            webbrowser.get().open(file_location, new=1)
+        else:
+            PlotDBStructure.network.show('graph.html')
+
